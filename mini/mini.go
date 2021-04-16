a13c9672ccd05aba9503c219e637e0c1eca13c2a
kprince <461019665@qq.com>
2021-04-16 17:04:44 +0800

    小程序添加security.msgSecCheck

--- mini/mini.go
+++ mini/mini.go
@@ -7,6 +7,7 @@ import (
 
 const (
 	aQrUrl = "https://api.weixin.qq.com/wxa/getwxacode"
+	aContentUrl = "https://api.weixin.qq.com/wxa/msg_sec_check"
 )
 
 type Mini struct {
@@ -15,6 +16,18 @@ type Mini struct {
 
 func (m Mini) GetAccessToken()(string,error){
 	return m.Context.GetAccessToken()
+}
+
+//普通小程序码，个数限制100000个，path限制128字节，宽度：[280,1280]
+func (m Mini) FilterContent(content string)([]byte,error){
+	params := map[string]interface{}{
+		"content":content,
+	}
+	response, err := m.HTTPPostJSONWithAccessToken(aQrUrl, &params)
+	if err != nil {
+		log.Println(err)
+	}
+	return response,err
 }
 
 //普通小程序码，个数限制100000个，path限制128字节，宽度：[280,1280]
@@ -34,6 +47,7 @@ func (m Mini) GetMiniCode(path string,width int, isHyaline bool)([]byte,error){
 	}
 	return response,err
 }
+
 
 //小程序码，个数无限制，scene限制32字符，宽度：[280,1280]
 //scene:最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
